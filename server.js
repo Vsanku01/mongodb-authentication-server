35d3ba09e0f0996268edf9c4027d86fba27aa681
Vsanku01 <ethhack001@gmail.com>
2020-11-03 13:51:38 +0530

    depoyed :rocket:

--- server.js
+++ server.js
@@ -54,4 +54,12 @@ mongoose.connect(
 );
 
 // Server Listening
-app.listen(process.env.port || 3000);
+// app.listen(process.env.port || 3000);
+
+app.listen(process.env.PORT || 5000, function () {
+  console.log(
+    'Express server listening on port %d in %s mode',
+    this.address().port,
+    app.settings.env
+  );
+});
